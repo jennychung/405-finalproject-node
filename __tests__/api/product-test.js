bcde91328aaeeb5b51726a5aeef2012e226ffa32
Jenny Chung <jennychung1@Jenny-Chung.lan1>
2019-05-02 13:42:15 -0700

    testing

---
+++ __tests__/api/product-test.js
@@ -0,0 +1,95 @@
+const frisby = require('frisby');
+
+const { Joi } = frisby;
+
+it('should return a status of 404 when the product does not exist', () => {
+  return frisby
+  .get('http://localhost:4000/api/products/-1')
+  .expect('status', 404);
+});
+
+it('should return a status of 200 when products exist', () => {
+  return frisby
+  .get('http://localhost:4000/api/products/')
+  .expect('status', 200);
+});
+
+it('should return a status of 200 when the product exists', () => {
+return frisby
+  .get('http://localhost:4000/api/products/10')
+  .expect('status', 200)
+  .expect('jsonTypes', {
+    id: Joi.number().required(),
+    name: Joi.string().required(),
+    brand: Joi.string().required(),
+    category: Joi.string().required(),
+    imageLink: Joi.string().required()
+});
+});
+
+it('should create product', () => {
+  return frisby
+    .post('http://localhost:4000/api/products', {
+     name: "Kale-lalu-yAHA",
+     brand: "Krave Beauty",
+     category: "Acid",
+     imageLink: "https://cdn.shopify.com/s/files/1/2090/8057/products/KRAVE_1-2_1_1024x1024.jpg?v=1550835534"
+    })
+    .expect('status', 422)
+    .expect('json', 'name', 'Kale-lalu-yAHA')
+    .expect('json', 'brand', 'Krave Beauty')
+    .expect('json', 'category', 'Acid')
+    .expect('json', 'imageLink', 'https://cdn.shopify.com/s/files/1/2090/8057/products/KRAVE_1-2_1_1024x1024.jpg?v=1550835534')
+    .expect('jsonTypes', 'id', Joi.number().required())
+});
+
+
+it('should return 200 when updating product successfully', () => {
+  return frisby
+    .patch('http://localhost:4000/api/products/355', {
+      id: 356,
+     name: "Kale-lalu-yAHA",
+     brand: "Krave Beauty",
+     category: "Acid",
+     imageLink: "https://cdn.shopify.com/s/files/1/2090/8057/products/KRAVE_1-2_1_1024x1024.jpg?v=1550835534"
+    })
+    .expect('status', 200)
+});
+
+it('should return a status of 404 when the product to be updated does not exist', () => {
+  return frisby
+    .patch('http://localhost:4000/api/products/-1', {
+      id: 356,
+     name: "Kale-lalu-yAHA",
+     brand: "Krave Beauty",
+     category: "Acid",
+     imageLink: "https://cdn.shopify.com/s/files/1/2090/8057/products/KRAVE_1-2_1_1024x1024.jpg?v=1550835534"
+    })
+    .expect('status', 404)
+});
+
+it('should return a status of 422 when the product to be updated has a validation error', () => {
+  return frisby
+    .patch('http://localhost:4000/api/products/100', {
+     id: 356,
+     name: "",
+     brand: "",
+     category: "",
+     imageLink: ""
+    })
+    .expect('status', 422)
+});
+
+
+
+it('should return 204 when deleting a product that exists', () => {
+  return frisby
+    .del('http://localhost:4000/api/products/100')
+    .expect('status', 204)
+});
+
+it('should return 404 when deleting a product that exists', () => {
+  return frisby
+    .del('http://localhost:4000/api/products/-1')
+    .expect('status', 404)
+});
---
+++ __tests__/unit/models/product-test.js
@@ -0,0 +1,73 @@
+const {
+  expect
+} = require('chai');
+const Track = require('./../../../models/product');
+
+describe('product', () => {
+  describe('name', () => {
+    it('shouldnt pass validation if there is no name', async () => {
+      try {
+        let product = new Product({
+          name: '',
+          brand: 'Krave Beauty',
+          category: 'Acid',
+          imageLink: 'https://cdn.shopify.com/s/files/1/2090/8057/products/KRAVE_1-2_1_1024x1024.jpg?v=1550835534'
+        });
+        await product.validate();
+      } catch (error) {
+        expect(error.errors[0].message).to.equal('Name is required');
+      }
+    });
+  });
+
+  describe('brand', () => {
+    it('shouldnt pass validation if there is no brand', async () => {
+      try {
+        let product = new Product({
+          name: 'Kale-lalu-yAHA',
+          brand: '',
+          category: 'Acid',
+          imageLink: 'https://cdn.shopify.com/s/files/1/2090/8057/products/KRAVE_1-2_1_1024x1024.jpg?v=1550835534'
+
+        });
+        await product.validate();
+      } catch (error) {
+        expect(error.errors[0].message).to.equal('Brand is required');
+      }
+    });
+  });
+
+  describe('category', () => {
+    it('shouldnt pass validation if there is no category', async () => {
+      try {
+        let product = new Product({
+          name: 'Kale-lalu-yAHA',
+          brand: 'Krave Beauty',
+          category: '',
+          imageLink: 'https://cdn.shopify.com/s/files/1/2090/8057/products/KRAVE_1-2_1_1024x1024.jpg?v=1550835534'
+
+        });
+        await product.validate();
+      } catch (error) {
+        expect(error.errors[0].message).to.equal('Category is required');
+      }
+    });
+  });
+
+  describe('imageLink', () => {
+    it('shouldnt pass validation if there is no image link', async () => {
+      try {
+        let product = new Product({
+          name: 'Kale-lalu-yAHA',
+          brand: 'Krave Beauty',
+          category: 'Acid',
+          imageLink: ''
+
+        });
+        await product.validate();
+      } catch (error) {
+        expect(error.errors[0].message).to.equal('Image is required');
+      }
+    });
+  });
+});
